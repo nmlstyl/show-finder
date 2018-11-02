7c0f173775e2dc14b8586031d2377886a0c01a4a
nmlstyl <joey.michalina.mariano@gmail.com>
2018-11-02 19:33:24 -0400

    added button

--- client/src/components/artists/artist.js
+++ client/src/components/artists/artist.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
 const Artist = (props) => {
+
+    const onTour = () => {
+      if (props.touringUntil !== null){
+          return  <p>On Tour Until: {props.touringUntil} <button>Find Shows</button></p>
+      } else {
+        return <p>No Shows Found</p>
+      }
+    }
+
     return(
       <div class="artist">
-        { props.name }<br></br>
-      { props.touringUntil }
+        <h2>{ props.name }</h2>
+        { onTour() }
       </div>
     )
 }
