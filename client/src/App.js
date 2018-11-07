5da05616e5a48d91009ae9b7aaadd5614dd73368
nmlstyl <joey.michalina.mariano@gmail.com>
2018-11-07 00:22:22 -0500

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import QueryByNameContainer from './containers/query_by_name_container'
+import Navbar from './components/navbar'
 import { Route } from 'react-router-dom';
 
 
@@ -9,6 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <Route path="/artistsearch" component={QueryByNameContainer} />
       </div>
     );
---
+++ client/src/components/navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Navbar = () => {
+    return(
+      <div id="navbar">
+      </div>
+    )
+}
+
+export default Navbar
