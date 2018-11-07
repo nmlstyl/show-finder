d404a21342b44b816870048a2796576cea156a61
nmlstyl <joey.michalina.mariano@gmail.com>
2018-11-07 00:19:12 -0500

    added react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import QueryByNameContainer from './containers/query_by_name_container'
+import { Route } from 'react-router-dom';
 
 
 class App extends Component {
@@ -8,7 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <QueryByNameContainer />
+        <Route path="/artistsearch" component={QueryByNameContainer} />
       </div>
     );
   }
