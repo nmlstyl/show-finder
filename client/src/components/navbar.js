ca62f1204bbc0df4b95c7c08b6b03a1a065c5762
nmlstyl <joey.michalina.mariano@gmail.com>
2018-11-08 15:02:24 -0500

    fixed a few class to className

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -3,20 +3,20 @@ import React from 'react'
 const Navbar = () => {
     return(
         <header><h1><strong><em>S H O W &nbsp; F I N D E R</em></strong></h1>
-          <nav class="navbar">
-            <a href="/"><button type="button" class="btn btn-default navbar-btn">Home</button></a>
+          <nav className="navbar">
+            <a href="/"><button type="button" className="btn btn-default navbar-btn">Home</button></a>
 
             &nbsp;&nbsp;&nbsp;&nbsp;
 
-            <a href="/"><button type="button" class="btn btn-default navbar-btn">Login or Signup</button></a>
+            <a href="/"><button type="button" className="btn btn-default navbar-btn">Login or Signup</button></a>
 
             &nbsp;&nbsp;&nbsp;&nbsp;
 
-            <a href="/artistsearch"><button type="button" class="btn btn-default navbar-btn"><span class="glyphicon glyphicon-search" aria-hidden="true"></span> &nbsp; Search By Artist</button></a>
+            <a href="/artistsearch"><button type="button" className="btn btn-default navbar-btn"><span className="glyphicon glyphicon-search" aria-hidden="true"></span> &nbsp; Search By Artist</button></a>
 
             &nbsp;&nbsp;&nbsp;&nbsp;
 
-            <a href="/"><button type="button" class="btn btn-default navbar-btn"><span class="glyphicon glyphicon-heart-empty" aria-hidden="true"></span> &nbsp; My Saved Bands</button></a>
+            <a href="/"><button type="button" className="btn btn-default navbar-btn"><span className="glyphicon glyphicon-heart-empty" aria-hidden="true"></span> &nbsp; My Saved Bands</button></a>
           </nav>
         </header>
     )
