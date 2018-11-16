b21fc54497738a0d4020984aa3dbf842edb0d8ca
nmlstyl <joey.michalina.mariano@gmail.com>
2018-11-16 15:22:18 -0500

    adding saved_sonkick_artitsts to container

--- client/src/containers/saved_bands_container.js
+++ client/src/containers/saved_bands_container.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SongkickArtists from '../components/artists/songkick_artists'
 
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
@@ -6,15 +7,22 @@ import * as actions from '../actions/act'
 
 class SavedBandsContainer extends Component {
 
+  componentDidMount(){
+
+  }
+
   render() {
-
     return (
-      <div>
+      <div id='savedbands' class="row">
+        <div className="col-sm-6"><SongkickArtists songkickArtists={ this.props.savedSongkickArtists }
+                                                   getSongkickShows={ this.props.actions.getSongkickShows }
+                                                   cookieAccess={ this.props.cookieAccess }/></div>
+        <div className="col-md-6"><h2>Bandsintown Artists</h2>
 
+        </div>
       </div>
     )
   }
-
 }
 
 function mapStateToProps(rootReducer) {
