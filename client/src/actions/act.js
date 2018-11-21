6cad1ab9390b4891159192b51716cf96a0d30618
nmlstyl <joey.michalina.mariano@gmail.com>
2018-11-21 00:27:19 -0500

    getting rid of the static.json - didnt work for deploy

--- client/src/actions/act.js
+++ client/src/actions/act.js
@@ -2,7 +2,7 @@ import fetch from 'isomorphic-fetch';
 
 const songkick_key = process.env.REACT_APP_SONGKICK_API_KEY
 const bandsintown_app_id = process.env.REACT_APP_BANDSINTOWN_APP_ID
-const base_url = 'show-findr.herokuapp.com'
+const base_url = 'localhost:3001'
 
 export function fetchByArtist(artist_name) {
   return (dispatch) => {
