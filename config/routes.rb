f1b7963572433ef16adb6849f2df009c5f0b7198
nmlstyl <joey.michalina.mariano@gmail.com>
2018-11-25 15:00:51 -0500

    adding some stuff for heroku and react router

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   end
 
   get 'login', to: 'users#login'
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
