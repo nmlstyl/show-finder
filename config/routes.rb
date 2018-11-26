1dc5f054096fc1e53f7e77ab6962fc60ae1e6c82
nmlstyl <joey.michalina.mariano@gmail.com>
2018-11-26 00:21:16 -0500

    trying again

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ Rails.application.routes.draw do
     !request.xhr? && request.format.html?
   end
 
-  get '', to: "application#fallback_index_html", constraints: ->(request) do
+  get '/', to: "application#fallback_index_html", constraints: ->(request) do
     !request.xhr? && request.format.html?
   end
 end
