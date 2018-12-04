d94be85663e7ac9d01f43b44deb5ef17bfb23270
nmlstyl <joey.michalina.mariano@gmail.com>
2018-12-04 01:19:41 -0500

    fleshed out the like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,25 @@
 class LikesController < ApplicationController
 
-  def index
-
+  def create
+    @like = Like.create(like_params)
+    render json: @like
   end
-  
+
+  def update
+    @like = Like.update(like_params)
+    render json: @like
+  end
+
+  private
+
+  def request_to_params
+    data = JSON.parse(request.raw_post)
+    params[:artist_id] = data['artist_id']
+    params[:user_id] = data['user_id']
+    params[:switch] = data['switch']
+  end
+
+  def like_params
+     params.permit(:artist_id, :user_id, :switch)
+  end
 end
