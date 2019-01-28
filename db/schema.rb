fbad1c8cad0fc7eb7dd8791dc2f43a9727c4c192
joeymariano <joey.michalina.mariano@gmail.com>
2019-01-28 12:48:49 -0500

    added facebook_id column to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181204060109) do
+ActiveRecord::Schema.define(version: 20190128174614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20181204060109) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "facebook_id"
   end
 
   create_table "users_artists", force: :cascade do |t|
