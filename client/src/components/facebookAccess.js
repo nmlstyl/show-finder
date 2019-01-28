367e989b02fb86e612cd9a777794512b28306fb7
joeymariano <joey.michalina.mariano@gmail.com>
2019-01-28 18:13:47 -0500

    sending fb id to back end

--- client/src/components/facebookAccess.js
+++ client/src/components/facebookAccess.js
@@ -4,14 +4,13 @@ import FacebookLogin from 'react-facebook-login';
 class FacebookAccess extends Component {
 
   responseFacebook = (response, loginOrSignupThroughApi) => {
-    debugger
-    console.log(response);
+    loginOrSignupThroughApi({ email: response.email, password: null, facebookId: response.id });
   }
 
   render(){
     return (
       <FacebookLogin appId="1396912973772758"
-             autoLoad={true}
+             autoLoad={ true }
              fields="name,email,picture"
              onClick={ this.componentClicked }
              callback={ (resp) => this.responseFacebook(resp, this.props.action) }
@@ -19,8 +18,6 @@ class FacebookAccess extends Component {
              icon="fa-facebook"/>
     )
   }
-
-
 }
 
 export default FacebookAccess
