97225baabbea0e7ae1aa6d0586ac714a7e0705fb
joeymariano <joey.michalina.mariano@gmail.com>
2019-03-03 17:33:44 -0500

    added redux devtools

--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,15 @@ import App from './App'
 import rootReducer from './reducers/index'
 
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 
 import { CookiesProvider } from 'react-cookie'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer,
+              compose(applyMiddleware(thunk),
+                      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) )
 
 ReactDOM.render(
 <CookiesProvider>
